44b01c5ddc2d8fc4035e825a37d0061d01c47434
Your Name <you@example.com>
2022-08-12 16:46:51 +0800

    更新检查是否存在挂载点，不存在则创建

--- auto_mount.sh
+++ auto_mount.sh
@@ -11,7 +11,18 @@ if [ -e /dev/sda ]; then
     else
         echo "USB is not mounted"
         #Mount the USB
-        mount /dev/sda1 /mnt/usb
+        # check mount point is available
+        if [ -e /mnt/usb ]; then
+            echo "USB is already mounted"
+        else
+            echo "USB is not mounted"
+            # create mount point
+            mkdir /mnt/usb
+            
+            #Mount the USB
+            mount /dev/sda /mnt/usb
+            echo "USB is mounted"
+        fi
     fi
 else
     echo "USB is not connected"
